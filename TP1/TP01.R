0f21fb0ceea54521e74e903a87cd3ef1d6ef1c54
Mohamed AIT MANSOUR <mohamedaitmansour95@gmail.com>
2018-09-14 09:07:24 +0200

    init project

---
+++ TP1/TP01.R
@@ -0,0 +1,8 @@
+#********************************************
+#* Name : AIT MANSOUR Mohamed               *
+#* Formation : M2-ILSEN                     *
+#* Group : Alternance                       *
+#* UCE : Outils pour l'apprentissage Auto.  *
+#* TP : 1                                   *
+#********************************************
+

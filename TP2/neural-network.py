fa50377dd08ebdc123eb1457b4b514c787b456fc
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-10-11 16:56:55 +0200

    init TP2

---
+++ TP2/neural-network.py
@@ -0,0 +1,7 @@
+#********************************************
+#* Name : AIT MANSOUR Mohamed               *
+#* Formation : M2-ILSEN                     *
+#* Group : Alternance                       *
+#* UCE : Outils pour l'apprentissage Auto.  *
+#* TP : 2                                   *
+#********************************************

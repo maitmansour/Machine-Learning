e4448c763f03c34e073ae26d8d071def0f4cfa05
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-10-12 09:11:06 +0200

    create and compile neuron layers

--- TP2/neural-network.py
+++ TP2/neural-network.py
@@ -5,3 +5,18 @@
 #* UCE : Outils pour l'apprentissage Auto.  *
 #* TP : 2                                   *
 #********************************************
+
+# import libraries
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
+# Create neural network
+model = Sequential()
+model.add(Dense(200,input_dim=400, activation='relu'))
+model.add(Dense(200,input_dim=400, activation='relu'))
+model.add(Dense(10,input_dim=400, activation='softmax'))
+
+
+# Compilation
+model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])

d587dfb445e4b9b691e5286be29ef4556195c1c1
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-10-12 09:18:04 +0200

    Load numbers dataset

--- TP2/neural-network.py
+++ TP2/neural-network.py
@@ -11,12 +11,17 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 
+# import mnist dataset
+from keras.datasets import mnist
+
 # Create neural network
 model = Sequential()
 model.add(Dense(200,input_dim=400, activation='relu'))
 model.add(Dense(200,input_dim=400, activation='relu'))
 model.add(Dense(10,input_dim=400, activation='softmax'))
 
+# Load mnist dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # Compilation
 model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])

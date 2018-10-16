66321ec6d00449a27ace69b48941ed959aa7375e
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-10-16 13:19:33 +0200

    final score

--- TP2/neural-network.py
+++ TP2/neural-network.py
@@ -55,3 +55,6 @@ history = model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+# Test loss: 0.4211650045061563
+# Test accuracy: 0.9644

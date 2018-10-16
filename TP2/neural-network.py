2540baff4eaf6e3e1699562e990343ba379a08c5
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-10-16 13:13:08 +0200

    Finished with 0.96 acc

--- TP2/neural-network.py
+++ TP2/neural-network.py
@@ -10,18 +10,48 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
-
+from keras.utils import np_utils
 # import mnist dataset
 from keras.datasets import mnist
 
-# Create neural network
-model = Sequential()
-model.add(Dense(200,input_dim=400, activation='relu'))
-model.add(Dense(200,input_dim=400, activation='relu'))
-model.add(Dense(10,input_dim=400, activation='softmax'))
-
-# Load mnist dataset
+# Load mnist dataset (the data, shuffled and split between train and test sets)
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# format data ( Reshape Gives a new shape to an array without changing its data.)
+# This two lines mean that we create mini batches from data 
+# Dataset of 60,000 28x28 grayscale images of 10 fashion categories, along with a test set of 10,000 images. 
+# This dataset can be used as a drop-in replacement for MNIST.
+
+#Train on 60000 samples, validate on 10000 samples
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+
+
+# convert class vectors to binary class matrices
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+# Create neural network
+# 28*28 pixels = 784 = number of inputs
+
+model = Sequential()
+
+# It means 784 input parameters, with 90 neurons in the FIRST hidden layer.
+model.add(Dense(90,input_dim=784, activation='relu'))
+model.add(Dense(90, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 
 # Compilation
 model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+
+# Using batches to get better results
+history = model.fit(x_train, y_train,
+					batch_size=490,
+                    epochs=25,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
